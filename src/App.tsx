5877fce45909c75e86f6b9594b6b1bbd14591c0b
Anurag Das <anuragdas2001>
2024-06-03 18:09:29 +0530

    Navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 // import { Navbar } from "./components/Navbar";
 import { router } from "./routes";
 import { RouterProvider } from "react-router-dom";
+import { DarkModeProvider } from "./context/DarkModeContext";
 const App: React.FC = () => {
-  return <>
-    {/* <Navbar/> */}
-  <RouterProvider router={router}/>
-  </>;
+  return (<>
+   
+    <DarkModeProvider>
+      <RouterProvider router={router}/>
+    </DarkModeProvider>
+  
+  </>);
 };
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDarkMode } from "../context/DarkModeContext";
+
 const navlinks = [
   { name: "Home", link: "/", img: "/haunted-house.png" },
   { name: "Genre", link: "/genre", img: "/genre.png" },
@@ -8,6 +10,8 @@ const navlinks = [
   // { name: "Recommendations", link: "/recommendations" },
 ];
 export const Navbar: React.FC = () => {
+  const {isDark,toggleTheme} = useDarkMode();
+
   return (
     <>
       <div className="h-10 flex justify-center">
@@ -29,7 +33,7 @@ export const Navbar: React.FC = () => {
             </Link>
           ))}
 
-          {/* <label className="swap swap-rotate">
+          <label className="swap swap-rotate">
             <input
               type="checkbox"
               checked={isDark}
@@ -52,7 +56,7 @@ export const Navbar: React.FC = () => {
             >
               <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
             </svg>
-          </label> */}
+          </label>
         </ul>
       </div>
     </>
