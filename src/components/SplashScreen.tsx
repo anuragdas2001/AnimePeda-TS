92cc74196f1fd3a70468f6e07930a7b821058ee4
Anurag Das <anuragdas2001>
2024-06-04 23:32:43 +0530

    Splash Screen bugs

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -4,16 +4,16 @@ import { useNavigate } from "react-router-dom";
 export const SplashScreen: React.FC = () => {
   const words1 = `Animepedia`;
   const words2 = `For anime lovers by anime lovers`;
-  const navigate = useNavigate();
-    useEffect(() => {
-      setTimeout(() => {
-        navigate("/signup");
-      }, 3000);
-    }, []);
+//   const navigate = useNavigate();
+//     useEffect(() => {
+//       setTimeout(() => {
+//         navigate("/signup");
+//       }, 3000);
+//     }, []);
 
   return (
     <>
-      <div className="bg-black">
+      <div className="bg-black border-2 border-red-600 h-screen">
         <div className="top-0 float-end p-4 w-5/12 h-screen">
           <img src="/DeathNoteSplash.png" alt="" className=" w-full h-full ml-8" />
         </div>
