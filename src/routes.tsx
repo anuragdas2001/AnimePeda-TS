89d0fd13d5d2216aa5585a1fd1ccc56cc02a458e
Anurag Das <anuragdas2001>
2024-06-04 12:33:49 +0530

    Manga done

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { Home } from "./components/Home";
+import { Manga } from "./components/Manga";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Navbar />,
-    children:[
-      {path: "/", element: <Home />},
-
-    ]
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/manga", element: <Manga /> },
+    ],
   },
 ]);
