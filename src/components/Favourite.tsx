92e0ecd1c597bcd8831ec650f703d3011dfcaf22
Anurag Das <anuragdas2001>
2024-06-06 15:16:24 +0530

    Favourites done

--- src/components/Favourite.tsx
+++ src/components/Favourite.tsx
@@ -1,28 +1,10 @@
 import React from "react";
 import { useFavourite } from "../context/FavouriteContext";
-import { createClient } from "@supabase/supabase-js";
+
 export const Favourite: React.FC = () => {
   const { favourites } = useFavourite();
   console.log(favourites);
-//   const SUPABASE_URL = "https://mvoltmxzeywbmfdgasdu.supabase.co";
-//   const SUPABASE_ANON_KEY =
-//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12b2x0bXh6ZXl3Ym1mZGdhc2R1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyMjg5MDAsImV4cCI6MjAzMTgwNDkwMH0.rsvLTGJvoA4x7XHMiwaFq8jMZk0cJO9PIG5SWpTBnWQ";
-//   const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-//   // Create a function to handle inserts
-//   const handleInserts = (payload:any) => {
-//     console.log("Change received!", payload);
-//   };
-
-//   // Listen to inserts
-//   supabase
-//     .channel("todos")
-//     .on(
-//       "postgres_changes",
-//       { event: "INSERT", schema: "public", table: "todos" },
-//       handleInserts
-//     )
-//     .subscribe();
   return (
     <>
       <div>
